7803eab96510efdacfa16019a87315532fa21774
DESKTOP-R4KNH9C\Tofunmi <tofunmiseun@gmail.com>
2018-01-13 16:46:20 +0100

    added package.json deployment bug fixes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-app.use(express.static('public'))
-app.get('/', (req, res) => res.sendFile('C:\\version-control\\water-marks\\index.html'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.listen(3000, "127.0.0.1", () => console.log('Example app listening on port 3000!'));
