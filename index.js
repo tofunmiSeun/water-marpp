e877c1695e1103afac4b075865d270e69caf0d3f
DESKTOP-R4KNH9C\Tofunmi <tofunmiseun@gmail.com>
2018-01-13 16:56:07 +0100

    ...

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-app.listen(PORT, "127.0.0.1", () => console.log('Example app listening on port ${PORT}!'));
+app.listen(process.env.PORT || 5000, "127.0.0.1", () => console.log('Example app listening on port ${PORT}!'));
