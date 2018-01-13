e19d711c44256bd0d6bad7ce2c9660673e4e675b
DESKTOP-R4KNH9C\Tofunmi <tofunmiseun@gmail.com>
2018-01-13 16:52:33 +0100

    added .env file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-app.listen(5000, "127.0.0.1", () => console.log('Example app listening on port 5000!'));
+app.listen(PORT, "127.0.0.1", () => console.log('Example app listening on port ${PORT}!'));
